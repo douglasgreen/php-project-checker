320e9b73dda83ddf9223f0a6a12b7746297c8946
Douglas S. Green <douglas.s.green@protonmail.com>
2026-06-01 09:33:35 -0400

    supporting PHP 8.5

--- templates/php/rector.php
+++ templates/php/rector.php
@@ -1,6 +1,7 @@
 <?php
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 use Rector\ValueObject\PhpVersion;
@@ -12,7 +13,6 @@ return RectorConfig::configure()
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    ->withPhpSets(php83: true)
     ->withPreparedSets(
         deadCode: true,
         codeQuality: true,
@@ -21,6 +21,9 @@ return RectorConfig::configure()
         earlyReturn: true,
         phpunitCodeQuality: true,
     )
+    ->withSets([
+        LevelSetList::UP_TO_PHP_85,
+    ])
     ->withComposerBased(
         symfony: true,
     )
